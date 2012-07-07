0041652a3d2141743bc96b40c1fef095bf8397b9
maerzbow <maerzbow@gmail.com>
2012-07-07 12:36:31 +0200

    updated command help

--- lib/lingohub/commands/help.rb
+++ lib/lingohub/commands/help.rb
@@ -61,7 +61,7 @@ module Lingohub::Command
         group.command 'translation:down --all --directory <path> --project <name>',                                          'download all resource files'
         group.command 'translation:down --locale <iso2_slug> --all --directory <path> --project <name>',                     'download all resource files, using the given locale as filter'
         group.command 'translation:down <file1> <file2> ... --directory <path> --project <name>',                            'download specific resource files'
-        group.command 'translation:up <file1> <file2> ... --locale <iso2_slug> --project <name>',                            'upload specific resource files'
+        group.command 'translation:up <file1> <file2> ... --locale <iso2_slug> --project <name>',                            'upload specific resource files, a locale may be specified to tell lingohub the locale of file content'
         group.space
       end
     end
